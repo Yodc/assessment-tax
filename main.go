f3cc29e5209ff58e023573fa96a117b0e610145e
YodC <68495500+Yodc@users.noreply.github.com>
2024-04-24 12:12:23 +0700

    add Graceful Shutdown

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 
 	godotenv.Load(".env")
 	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
 
-	e.Logger.Fatal(e.Start(os.Getenv("API_PORT")))
+	go func() {
+		if err := e.Start(os.Getenv("PORT")); err != nil && err != http.ErrServerClosed { // Start server
+			e.Logger.Fatal("shutting down the server")
+
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	fmt.Println("shutting down the server")
+
 }
